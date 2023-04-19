2466664aceaa4ef980c8c544609ed408f2b7f2f8
S0haibZafar <sohaibzafar71@gmail.com>
2023-04-19 21:37:38 +0500

    Create Add-todo component to add item in list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './MyComponents/todos/todos.component';
 import { TodoItemComponent } from './MyComponents/todo-item/todo-item.component';
+import { AddTodoComponent } from './MyComponents/add-todo/add-todo.component';
 // import { TodoItemComponent } from '../Mycomponents/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
-    TodoItemComponent
+    TodoItemComponent,
+    AddTodoComponent
   ],
   imports: [
     BrowserModule
