2f3be3afdcac044bfa374f2b12c8d3b283ba533e
S0haibZafar <sohaibzafar71@gmail.com>
2023-04-21 21:40:56 +0500

    Save todos in local storage

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -8,23 +8,31 @@ import { Todo } from 'src/app/Models/Todo';
 })
 export class TodosComponent implements OnInit {
 
+  localItem:string | null;
   todoData:Todo[];
 
   constructor() {
-    this.todoData=[
-      {
-        sno:1,
-        name:'ALi',
-        description:'Description',
-        active: true
-      },
-      {
-        sno:2,
-        name:'Khan',
-        description:'Description',
-        active: true
-      }
-    ]
+    this.localItem = localStorage.getItem("todo");
+    if(this.localItem == null){
+      this.todoData = []
+    }else{
+      this.todoData = JSON.parse(this.localItem)
+
+    }
+    // this.todoData=[
+      // {
+      //   sno:1,
+      //   name:'ALi',
+      //   description:'Description',
+      //   active: true
+      // },
+      // {
+      //   sno:2,
+      //   name:'Khan',
+      //   description:'Description',
+      //   active: true
+      // }
+    // ]
    }
 
   ngOnInit(): void {
@@ -39,6 +47,7 @@ export class TodosComponent implements OnInit {
   addItem(todo:Todo){
     console.log("Add Todo::", todo)
     this.todoData.push(todo); 
+    localStorage.setItem("todo",JSON.stringify(this.todoData))
   }
 
 }
